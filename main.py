aa7c0d598e00a02087792eb3eb03564da7c0433d
Andy Wang <therainruffy@yahoo.com>
2026-07-21 13:15:57 -0400

    add cli args to main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import pandas as pd
 from gene_functions import annotate_peaks
@@ -6,22 +7,38 @@ GFF_HEADERS=['seqid', 'source', 'type', 'start', 'end', 'score', 'direction', 'p
 
 
 if __name__ == '__main__':
-    with open('config.json', 'r') as f:
+    
+    parser = argparse.ArgumentParser(description="Main script to annotate ChIP-seq peaks.")
+    parser.add_argument("-c", "--config", default="config.json", help="Path to configuration JSON file (default: config.json)")
+    parser.add_argument("-g", "--genes", help="Path to gene annotation GFF file")
+    parser.add_argument("-p", "--peaks", help="Path to peaks GFF file")
+    parser.add_argument("-o", "--output", help="Output CSV filename")
+
+    args = parser.parse_args()
+    
+    config_path = args.config
+    with open(config_path, 'r') as f:
         config = json.load(f)
 
-    MAB_GFF_PATH = config.get('paths', {}).get('genes', 'MabATCC19977_gff.gff')
+    mab_gff_path = args.genes or config.get('paths', {}).get('genes')
+    if not mab_gff_path:
+        print("You didn't pass in a path to the gene Mab gff.")
+        exit(1)
     try:
-        mab_df = pd.read_table(MAB_GFF_PATH, header=None, names=GFF_HEADERS)
+        mab_df = pd.read_table(mab_gff_path, header=None, names=GFF_HEADERS)
     except FileNotFoundError:
-        print(f"Error: The file '{MAB_GFF_PATH}' was not found. Please check the path in the config.json file.")
+        print(f"Error: The file '{mab_gff_path}' was not found. Please check the path in the config.json file.")
         exit(1)
     print("Successfully loaded MAB GFF file with column names:", GFF_HEADERS)
 
-    PEAKS_GFF_PATH = config.get('paths', {}).get('peaks', 'SigHP1/SigHP1_FDR0.01combo.gff')
+    peaks_gff_path = args.peaks or config.get('paths', {}).get('peaks')
+    if not peaks_gff_path:
+        print("You didn't pass in a path to the peaks gff.")
+        exit(1)
     try:
-        peaks_df = pd.read_table(PEAKS_GFF_PATH, header=None, names=GFF_HEADERS)
+        peaks_df = pd.read_table(peaks_gff_path, header=None, names=GFF_HEADERS)
     except FileNotFoundError:
-        print(f"Error: The file '{PEAKS_GFF_PATH}' was not found. Please check the path in the config.json file.")
+        print(f"Error: The file '{peaks_gff_path}' was not found. Please check the path in the config.json file.")
         exit(1)
     print("Successfully loaded peaks GFF file with column names:", GFF_HEADERS)
 
@@ -30,6 +47,6 @@ if __name__ == '__main__':
     print("Output preview:")
     print(regulated_peaks.head())
     
-    out_path = config.get('paths', {}).get('output', 'regulated_peaks_out.csv')
+    out_path = args.output or config.get('paths', {}).get('output', 'regulated_peaks_out.csv')
     regulated_peaks.to_csv(out_path, index=False)
     print("Saved analysis to", out_path)
